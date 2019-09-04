912a303b1be7424ccd5c5ef4c8354a7a8c8a701b
playrey2010 <playrey2010@gmail.com>
2019-09-04 11:34:15 -0400

    Display two variables

---
+++ src/NumberVariables.java
@@ -0,0 +1,8 @@
+public class NumberVariables {
+    public static void main(String[] args) {
+        Integer mynumber = 2;
+        Double myothernumber = 1.7938;
+        System.out.println("Variable mynumber has a value of " + mynumber
+                            + "\nVariable myothernumber has a value of " + myothernumber);
+    }
+}
